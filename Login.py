68e2a82dcd08f348d6588e92ffc1461bcb4219e5
anasadil33 <70198928+anasadil33@users.noreply.github.com>
2021-10-04 18:08:19 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,19 +1,19 @@
 
-
+#user id and password stored in the system to compare
 userid = '1234';
 password = '1234';
-
+#take input from the user 
 username = str(input("Please Enter Username\n"));
-if username == userid :
+if username == userid :                                   #if username matches, then proceed
     print("Please enter Password\n");
     password2 = str(input());
-    if password2 == password :
+    if password2 == password :                            #if password matches then login successful or else invalid password
         print ('Password is correct\n');
         print("Login succesfull!\n");
     else :
-        print("Enter password again!\n");
         print("Wrong password detected.\n");
-else :
+        print("Enter password again!\n");
+else :                                                    # default case. 
     print("Enter correct username!");
 
     
